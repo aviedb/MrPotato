10f4b4998c05d4a1f8128af20770b66859458e92
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2018-09-17 23:44:47 +0700

    Update chips can now toggle

--- app/src/main/java/men/ngopi/aviedb/mrpotato/MainActivity.java
+++ app/src/main/java/men/ngopi/aviedb/mrpotato/MainActivity.java
@@ -1,8 +1,12 @@
 package men.ngopi.aviedb.mrpotato;
 
 import android.graphics.Typeface;
+import android.support.design.chip.Chip;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,17 +27,115 @@ public class MainActivity extends AppCompatActivity {
         TextView placeholder = findViewById(R.id.placeholder);
         placeholder.setTypeface(typeface);
 
-        // Declaring each chips' Id
-        android.support.design.chip.Chip hat_chip = findViewById(R.id.hat_chip);
-        android.support.design.chip.Chip nose_chip = findViewById(R.id.nose_chip);
-        android.support.design.chip.Chip arms_chip = findViewById(R.id.arms_chip);
-        android.support.design.chip.Chip eyes_chip = findViewById(R.id.eyes_chip);
-        android.support.design.chip.Chip glasses_chip = findViewById(R.id.glasses_chip);
-        android.support.design.chip.Chip eyebrows_chip = findViewById(R.id.eyebrows_chip);
-        android.support.design.chip.Chip ears_chip = findViewById(R.id.ears_chip);
-        android.support.design.chip.Chip mouth_chip = findViewById(R.id.mouth_chip);
-        android.support.design.chip.Chip mustache_chip = findViewById(R.id.mustache_chip);
-        android.support.design.chip.Chip shoes = findViewById(R.id.shoes_chip);
+        // Toggle hat visibility
+        Chip hat_chip = findViewById(R.id.hat_chip);
+        hat_chip.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+                ImageView hat = findViewById(R.id.hat);
+
+                hat.setVisibility(isChecked? View.VISIBLE : View.INVISIBLE);
+            }
+        });
+
+        // Toggle nose visibility
+        Chip nose_chip = findViewById(R.id.nose_chip);
+        nose_chip.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+                ImageView nose = findViewById(R.id.nose);
+
+                nose.setVisibility(isChecked? View.VISIBLE : View.INVISIBLE);
+            }
+        });
+
+        // Toggle arms visibility
+        Chip arms_chip = findViewById(R.id.arms_chip);
+        arms_chip.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+                ImageView arms = findViewById(R.id.arms);
+
+                arms.setVisibility(isChecked? View.VISIBLE : View.INVISIBLE);
+            }
+        });
+
+        // Toggle eyes visibility
+        Chip eyes_chip = findViewById(R.id.eyes_chip);
+        eyes_chip.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+                ImageView eyes = findViewById(R.id.eyes);
+
+                eyes.setVisibility(isChecked? View.VISIBLE : View.INVISIBLE);
+            }
+        });
+
+        // Toggle glasses visibility
+        Chip glasses_chip = findViewById(R.id.glasses_chip);
+        glasses_chip.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+                ImageView glasses = findViewById(R.id.glasses);
+
+                glasses.setVisibility(isChecked? View.VISIBLE : View.INVISIBLE);
+            }
+        });
+
+        // Toggle eyebrows visibility
+        Chip eyebrows_chip = findViewById(R.id.eyebrows_chip);
+        eyebrows_chip.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+                ImageView eyebrows = findViewById(R.id.eyebrows);
+
+                eyebrows.setVisibility(isChecked? View.VISIBLE : View.INVISIBLE);
+            }
+        });
+
+        // Toggle ears visibility
+        Chip ears_chip = findViewById(R.id.ears_chip);
+        ears_chip.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+                ImageView ears = findViewById(R.id.ears);
+
+                ears.setVisibility(isChecked? View.VISIBLE : View.INVISIBLE);
+            }
+        });
+
+        // Toggle mouth visibility
+        Chip mouth_chip = findViewById(R.id.mouth_chip);
+        mouth_chip.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+                ImageView mouth = findViewById(R.id.mouth);
+
+                mouth.setVisibility(isChecked? View.VISIBLE : View.INVISIBLE);
+            }
+        });
+
+        // Toggle mustache visibility
+        Chip mustache_chip = findViewById(R.id.mustache_chip);
+        mustache_chip.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+                ImageView mustache = findViewById(R.id.mustache);
+
+                mustache.setVisibility(isChecked? View.VISIBLE : View.INVISIBLE);
+            }
+        });
+
+        // Toggle shoes visibility
+        Chip shoes = findViewById(R.id.shoes_chip);
+        shoes.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+                ImageView shoes = findViewById(R.id.shoes);
+
+                shoes.setVisibility(isChecked? View.VISIBLE : View.INVISIBLE);
+            }
+        });
 
         // Changing each chips' font
         hat_chip.setTypeface(typeface);
