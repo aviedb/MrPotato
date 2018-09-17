9304149536ec0cb6a411c0400335eef9858e76d5
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2018-09-17 22:49:46 +0700

    Update chips

--- app/src/main/java/men/ngopi/aviedb/mrpotato/MainActivity.java
+++ app/src/main/java/men/ngopi/aviedb/mrpotato/MainActivity.java
@@ -19,19 +19,32 @@ public class MainActivity extends AppCompatActivity {
         TextView mr_potato = findViewById(R.id.mr_potato);
         mr_potato.setTypeface(typeface);
 
-        // Change chips font
+        // Changing placeholder font
+        TextView placeholder = findViewById(R.id.placeholder);
+        placeholder.setTypeface(typeface);
+
+        // Declaring each chips' Id
         android.support.design.chip.Chip hat_chip = findViewById(R.id.hat_chip);
-        android.support.design.chip.Chip eyebrows_chip = findViewById(R.id.eyebrows_chip);
         android.support.design.chip.Chip nose_chip = findViewById(R.id.nose_chip);
-        android.support.design.chip.Chip mustache_chip = findViewById(R.id.mustache_chip);
         android.support.design.chip.Chip arms_chip = findViewById(R.id.arms_chip);
         android.support.design.chip.Chip eyes_chip = findViewById(R.id.eyes_chip);
+        android.support.design.chip.Chip glasses_chip = findViewById(R.id.glasses_chip);
+        android.support.design.chip.Chip eyebrows_chip = findViewById(R.id.eyebrows_chip);
+        android.support.design.chip.Chip ears_chip = findViewById(R.id.ears_chip);
+        android.support.design.chip.Chip mouth_chip = findViewById(R.id.mouth_chip);
+        android.support.design.chip.Chip mustache_chip = findViewById(R.id.mustache_chip);
+        android.support.design.chip.Chip shoes = findViewById(R.id.shoes_chip);
 
+        // Changing each chips' font
         hat_chip.setTypeface(typeface);
-        eyebrows_chip.setTypeface(typeface);
         nose_chip.setTypeface(typeface);
-        mustache_chip.setTypeface(typeface);
         arms_chip.setTypeface(typeface);
         eyes_chip.setTypeface(typeface);
+        glasses_chip.setTypeface(typeface);
+        eyebrows_chip.setTypeface(typeface);
+        ears_chip.setTypeface(typeface);
+        mouth_chip.setTypeface(typeface);
+        mustache_chip.setTypeface(typeface);
+        shoes.setTypeface(typeface);
     }
 }
