a03200c4ef1e907ecb3394210cd76d0208fb6009
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2018-09-21 09:25:46 +0700

    Update

--- app/src/main/java/men/ngopi/aviedb/mrpotato/SplashActivity.java
+++ app/src/main/java/men/ngopi/aviedb/mrpotato/SplashActivity.java
@@ -30,7 +30,7 @@ public class SplashActivity extends AppCompatActivity {
         Thread timer = new Thread(){
             public void run(){
                 try {
-                    sleep(3000);
+                    sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
